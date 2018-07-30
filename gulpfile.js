76970f62f3c65ec0819117b15f7e4c805d195f6b
Casprine Assempah <casprine.001@gmail.com>
2018-07-30 23:11:18 +0000

    added some few updates

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,9 @@ var paths = {
     build: "./public/views/"
   },
   sass: {
-    src: "./src/assets/jello.scs"
+    src: "./style.scss",
+    srcBuild: "./public/",
+    watch: "./src/assets/sass/*/*.scss"
   }
 };
 
@@ -29,3 +31,32 @@ gulp.task("server", function() {
     port: 2018
   });
 });
+
+gulp.task("html", function() {
+  return gulp.src(path.index.src).pipe(connect.reload());
+});
+
+gulp.task("html:watch", function() {
+  gulp.watch(path.index.src, ["html"]);
+});
+
+gulp.task("views", function() {
+  return gulp.src(path.views.src).pipe(connect.reload());
+});
+
+gulp.task("views:watch", function() {
+  gulp.watch(path.views.src, ["views"]);
+});
+
+gulp.task("sass", function() {
+  return gulp
+    .src(paths.sass.src)
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("./"));
+});
+
+gulp.task("sass:watch", function() {
+  gulp.watch(path.sass.watch, ["sass"]);
+});
+
+gulp.task("dev", ["html", "html:watch", "views", "views:watch", "sass:watch"]);
