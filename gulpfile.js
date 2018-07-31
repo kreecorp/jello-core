cc5894da6da71c05fc67cdf7029f433969df2f04
Casprine Assempah <casprine.001@gmail.com>
2018-07-31 13:25:00 +0000

    added new gulp configs

--- gulpfile.js
+++ gulpfile.js
@@ -33,85 +33,47 @@ var paths = {
 // Local Server Config
 
 // Basic server on port:2018
-gulp.task("server", function() {
+
+const server = () => {
   connect.server({
     livereload: true,
     port: 2018
   });
-});
+};
 
-gulp.task("html", function() {
+const html = () => {
   return gulp.src(paths.index.src).pipe(connect.reload());
-});
+};
 
-gulp.task("html:watch", function() {
+const htmlServer = () => {
   gulp.watch(paths.index.src, ["html"]);
-});
+};
 
-gulp.task("views", function() {
+const views = () => {
   return gulp.src(paths.views.src).pipe(connect.reload());
-});
+};
 
-gulp.task("views:watch", function() {
+const viewsWatch = () => {
   gulp.watch(paths.views.src, ["views"]);
-});
+};
 
-gulp.task("sass", function() {
+const sassConverter = () => {
   return gulp
     .src(paths.sass.src)
     .pipe(sass().on("error", sass.logError))
     .pipe(gulp.dest("./"))
     .pipe(connect.reload());
-});
+};
 
-gulp.task("style:watch", function() {
-  gulp.watch(paths.sass.src, ["sass"]);
-});
+const styleWatch = () => {
+  gulp.watch(paths.sass.src, ["sassConverter"]);
+};
 
-gulp.task("sass:watch", function() {
-  gulp.watch(paths.sass.watch, ["sass"]);
-});
-gulp.task("jello", function() {
-  gulp.watch(paths.jello.index, ["sass"]);
-});
+const SassWatch = () => {
+  gulp.watch(paths.sass.watch, ["sassConverter"]);
+};
 
-gulp.task("dev", [
-  "server",
-  "html",
-  "html:watch",
-  "views",
-  "views:watch",
-  "sass",
-  "sass:watch",
-  "style:watch",
-  "jello"
-]);
-
-// Build Config
-
-gulp.task("index-build", function() {
-  return gulp
-    .src(paths.index.src)
-    .pipe(
-      htmlmin({
-        collapseWhitespace: true
-      })
-    )
-    .pipe(gulp.dest(paths.index.build));
-});
-
-gulp.task("css-build", function() {
-  return gulp
-    .src(paths.css.src)
-    .pipe(
-      autoprefixer({
-        browsers: ["last 2 versions"],
-        cascade: false
-      })
-    )
-    .pipe(cleanCSS({ compatibility: "ie8" }))
-    .pipe(gulp.dest(paths.css.build));
-});
-
-gulp.task("build", 
-["index-build", "sass", "css-build"]);
+const jello = () => {
+  gulp.watch(paths.jello.index, ["sassConverter"]);
+};
+gulp.task("dev", gulp.series(server, html, sassConverter , styleWatch ));
